403ee9245be5431909375fd5a011c6119fb915f0
marklion <marklion@sina.com>
2020-08-19 15:27:22 +0800

    add reg input filter

--- back-end/user_manage_rest_9098/user_manage/user_manage/src/db_sqlite_user.cpp
+++ back-end/user_manage_rest_9098/user_manage/user_manage/src/db_sqlite_user.cpp
@@ -1,19 +1,75 @@
 #include "db_sqlite_user.h"
 #include <sqlite3.h>
+
+static bool id_is_valid(std::string _id)
+{
+    bool bRet = false;
+
+    if (_id.length() == 11)
+    {
+        bool isallDig = true;
+        for (auto var : _id)
+        {
+            if (!isdigit(var))
+            {
+                isallDig = false;
+                break;
+            }
+        }
+        if (isallDig)
+        {
+            bRet = true;
+        }
+    }
+
+    return bRet;
+}
+static bool pwd_is_valid(std::string _pwd)
+{
+    bool bRet = false;
+
+    if (_pwd.length() > 0)
+    {
+        bRet = true;
+    }
+
+    return bRet;
+}
+static bool username_is_valid(std::string _username)
+{
+    bool bRet = false;
+
+    if (_username.length() > 0)
+    {
+        bRet = true;
+    }
+
+    return bRet;
+}
+
 bool db_sqlite_insert_user(std::string _id, std::string _pwd, std::string _username)
 {
     int db_ret = -1;
     bool bRet = false;
     sqlite3 *db = NULL;
 
-    db_ret = sqlite3_open("/database/pk_user", &db);
-    if (0 == db_ret)
+    bool bInputValid = true;
+    bInputValid |= id_is_valid(_id);
+    bInputValid |= pwd_is_valid(_pwd);
+    bInputValid |= username_is_valid(_username);
+
+    if (bInputValid)
     {
-        std::string sql_cmd = "insert into pk_user values ('" + _id + "', '" + _pwd + "', '" + _username + "');";
-        db_ret = sqlite3_exec(db, sql_cmd.c_str(), NULL, NULL, NULL);
-        if (SQLITE_OK ==  db_ret)
+        db_ret = sqlite3_open("/database/pk_user", &db);
+        if (0 == db_ret)
         {
-            bRet = true;
+            std::string sql_cmd = "insert into pk_user values ('" + _id + "', '" + _pwd + "', '" + _username + "');";
+            db_ret = sqlite3_exec(db, sql_cmd.c_str(), NULL, NULL, NULL);
+            if (SQLITE_OK == db_ret)
+            {
+                bRet = true;
+            }
+            sqlite3_close(db);
         }
     }
 
